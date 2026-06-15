6ff6c170a8d2d362f43e1d69cdbf2bd4e0474f78
Matheus Morais <moraism.dev@gmail.com>
2026-06-15 10:18:31 -0300

    feat: add global exception handler for validation errors

---
+++ src/main/java/io/github/MattheusMorais/LibraryAPI/controller/common/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package io.github.MattheusMorais.LibraryAPI.controller.common;
+
+import io.github.MattheusMorais.LibraryAPI.dto.error.AnswerErrorDTO;
+import io.github.MattheusMorais.LibraryAPI.dto.error.FieldErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
+    public AnswerErrorDTO handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldErrorDTO> errors = e.getFieldErrors()
+                .stream()
+                .map(fieldError -> new FieldErrorDTO(
+                        fieldError.getField(),
+                        fieldError.getDefaultMessage()))
+                .toList();
+
+        return new AnswerErrorDTO(
+                HttpStatus.UNPROCESSABLE_CONTENT.value(),
+                "Erro de validação",
+                errors
+        );
+    }
+}
